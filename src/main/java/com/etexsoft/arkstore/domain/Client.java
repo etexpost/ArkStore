f268e8511987c8db1c71a752b7391d8e66923017
etex.post@gmail.com <etex.post@gmail.com>
2016-01-20 22:56:00 +0200

    1

--- src/main/java/com/etexsoft/arkstore/domain/Client.java
+++ src/main/java/com/etexsoft/arkstore/domain/Client.java
@@ -1,7 +1,19 @@
 package com.etexsoft.arkstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+
 /**
  * Created by Андрей on 20.01.2016.
+ * Клиенты компании
+ *
  */
+
+@Entity
+@Table(name = "clients") /*Данные о клиентах гранятся в таблице clients */
 public class Client {
+    private String fullNane; /*Полное наименование клиента*/
+    private String shortName; /*Сокращенное наименование клиента*/
+    private ArrayList <String> = new ArrayList <String>;
 }
