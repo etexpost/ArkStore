3e81527062c7210863fdd869c96f7683087f5730
etexpost <etex.post@gmail.com>
2016-01-21 23:27:54 +0200

    11

--- src/main/java/com/etexsoft/arkstore/domain/Client.java
+++ src/main/java/com/etexsoft/arkstore/domain/Client.java
@@ -1,5 +1,7 @@
 package com.etexsoft.arkstore.domain;
 
+import sun.util.calendar.LocalGregorianCalendar;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.ArrayList;
@@ -10,9 +12,51 @@ import java.util.ArrayList;
  *
  */
 
+
 @Entity
 @Table(name = "clients") /*Дані про клієнтів зберігаються в таблиці clients */
 public class Client {
+
+    Client () {
+
+    }
+
+    private class ContactPerson {
+
+        private String surname; /*Прізвище контактної особи*/
+        private String name; /*Ім'я контактної особи*/
+        private String midName; /*По батькові контактної особи*/
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMidName() {
+            return midName;
+        }
+
+        public void setMidName(String midName) {
+            this.midName = midName;
+        }
+
+
+        @Override
+        public String toString() {
+            return "contactPerson{}" + "Прізвище: " +surname+ "Ім'я: "+ name + "По батькові: "+ midName;
+        }
+    }
 
     public boolean isClientType() {
         return clientType;
@@ -62,42 +106,22 @@ public class Client {
         this.manager = manager;
     }
 
-    private class contactPerson {
-
-        private String surname; /*Прізвище контактної особи*/
-        private String name; /*Ім'я контактної особи*/
-        private String midName; /*По батькові контактної особи*/
-
-        public String getSurname() {
-            return surname;
-        }
-
-        public void setSurname(String surname) {
-            this.surname = surname;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getMidName() {
-            return midName;
-        }
-
-        public void setMidName(String midName) {
-            this.midName = midName;
-        }
-
-
-        @Override
-        public String toString() {
-            return "contactPerson{}" + "Прізвище: " +surname+ "Ім'я: "+ name + "По батькові: "+ midName;
-        }
+    public LocalGregorianCalendar.Date getDateOfChange() {
+        return dateOfChange;
     }
+
+    public void setDateOfChange(LocalGregorianCalendar.Date dateOfChange) {
+        this.dateOfChange = dateOfChange;
+    }
+
+    public User getWhoMadeChanges() {
+        return whoMadeChanges;
+    }
+
+    public void setWhoMadeChanges(User whoMadeChanges) {
+        this.whoMadeChanges = whoMadeChanges;
+    }
+
 
     private boolean clientType; /* Тип клієнта - true = Юрособа -  false = Фізособа */
     private String edrpou; /*ЄДРПОУ юрособи*/
@@ -105,13 +129,34 @@ public class Client {
     private String fullNane; /*Повне найменування клієнта*/
     private String shortName; /*Скорочене найменування клієнта*/
     private User manager; /*Менеджер, який працює з клієнтом*/
+    private LocalGregorianCalendar.Date dateOfChange; /*Дата останніх змін даних про клієнта*/
+    private User whoMadeChanges; /*Хто вніс зміни до запису, або створив запис*/
     private ArrayList <String> phoneNumber = new ArrayList <String>(); /*Телефонні номери*/
     private ArrayList <String> cellNumber = new ArrayList <String>(); /*Мобільні номери*/
-    private ArrayList <contactPerson> clientContactPerson = new ArrayList <contactPerson>(); /*Список контактних осіб*/
+    private ArrayList <ContactPerson> clientContactPerson = new ArrayList <ContactPerson>(); /*Список контактних осіб*/
     private ArrayList <Client> historyOfChanges = new ArrayList<Client>(); /*Збереження історії змін даних про клієнта*/
 
-
-    Client () {
-
+    /*Додавання номеру телефона*/
+    public void addPhoneNumber(String phone) {
+        this.phoneNumber.add(phone);
     }
+
+    /*Додавання номеру мобільного*/
+    public void addCellNumber(String cellphone) {
+        this.cellNumber.add(cellphone);
+    }
+
+    /*Додавання контактноъ особи*/
+    public void addClientContactPerson(ContactPerson contactPerson) {
+        this.clientContactPerson.add(contactPerson);
+    }
+
+    public void addHistoryOfChanges(Client client) {
+        client.setDateOfChange();   /*Передати поточну дату*/
+        client.setWhoMadeChanges(); /*Передати поточного користувача*/
+        this.historyOfChanges.add(client);
+    }
+
+
+
 }
