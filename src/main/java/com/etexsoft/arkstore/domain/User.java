e42ec2d577854700097480a184a4c5d373dfe631
etex.post@gmail.com <etex.post@gmail.com>
2016-01-23 18:30:19 +0200

    1

--- src/main/java/com/etexsoft/arkstore/domain/User.java
+++ src/main/java/com/etexsoft/arkstore/domain/User.java
@@ -1,7 +1,109 @@
 package com.etexsoft.arkstore.domain;
 
+import sun.util.calendar.LocalGregorianCalendar;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Андрей on 20.01.2016.
  */
+
+@Entity
+@Table(name = "users") /*Дані про користувачів зберігаються в таблиці users */
 public class User {
+    User () {
+
+    }
+
+    public String getUserSurname() {
+        return UserSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        UserSurname = userSurname;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserMidName() {
+        return UserMidName;
+    }
+
+    public void setUserMidName(String userMidName) {
+        UserMidName = userMidName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalGregorianCalendar.Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalGregorianCalendar.Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalGregorianCalendar.Date getDateStartWork() {
+        return dateStartWork;
+    }
+
+    public void setDateStartWork(LocalGregorianCalendar.Date dateStartWork) {
+        this.dateStartWork = dateStartWork;
+    }
+
+    public LocalGregorianCalendar.Date getDateStopWork() {
+        return dateStopWork;
+    }
+
+    public void setDateStopWork(LocalGregorianCalendar.Date dateStopWork) {
+        this.dateStopWork = dateStopWork;
+    }
+
+    public boolean isAccessStatus() {
+        return accessStatus;
+    }
+
+    public void setAccessStatus(boolean accessStatus) {
+        this.accessStatus = accessStatus;
+    }
+
+    private String UserSurname; /*Прізвище користувача*/
+    private String UserName; /*Ім'я користувача*/
+    private String UserMidName; /*По батькові користувача*/
+    private String login; /*Логін користувача*/
+    private String password; /*Пароль користувача*/
+    private String status; /*статус - адміністратор, менеджер і т.юд.*/
+    private LocalGregorianCalendar.Date dateOfBirth; /*Дата народження*/
+    private LocalGregorianCalendar.Date dateStartWork; /*Дата початку роботи*/
+    private LocalGregorianCalendar.Date dateStopWork; /*Дата завершення роботи*/
+    private boolean accessStatus; /*статус доступу*/
 }
